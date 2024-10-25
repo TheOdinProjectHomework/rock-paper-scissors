c0d889d4a2a17a310d9ce5736018c2eb8500fa71
Cristian Contreras <contreras.cristian994@gmail.com>
2024-10-25 04:10:26 -0400

    updated logic to work with btns

--- script.js
+++ script.js
@@ -1,4 +1,41 @@
-console.log("Hello world!");
+console.log("Rock, Paper, Scissors game!");
+
+let humanScore = 0;
+let computerScore = 0;
+
+let playerScore = document.getElementById("player-h2");
+
+let p1Score = document.createElement("p");
+p1Score.textContent = humanScore;
+playerScore.appendChild(p1Score);
+
+let cpuScore = document.getElementById("computer-h2");
+
+let p2Score = document.createElement("p");
+p2Score.textContent = computerScore;
+cpuScore.appendChild(p2Score);
+
+let rockBtn = document.getElementById("rockBtn");
+let paperBtn = document.getElementById("paperBtn");
+let scissorsBtn = document.getElementById("scissorBtn");
+
+rockBtn.addEventListener("click", function() {
+    let humanChoice = "rock";
+    let computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+});
+
+paperBtn.addEventListener("click", function() {
+    let humanChoice = "paper";
+    let computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+});
+
+scissorsBtn.addEventListener("click", function() {
+    let humanChoice = "scissors";
+    let computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+});
 
 function getComputerChoice() {
     let random = Math.floor(Math.random() * 3);
@@ -18,45 +55,65 @@ function getHumanChoice() {
     return humanChoice;
 }
 
-let humanScore = 0;
-let computerScore = 0;
-
+function weHaveAwinner(playerScore, cpuScore) {
+    if(playerScore === 4) {
+        alert("You win!!!");
+        humanScore = 0;
+        computerScore = 0;
+        p1Score.textContent = humanScore;
+        p2Score.textContent = computerScore;
+    } else if(cpuScore === 4) {
+        alert("Computer wins!!!");
+        humanScore = 0;
+        computerScore = 0; 
+        p1Score.textContent = humanScore;
+        p2Score.textContent = computerScore;
+    }
+}
 
 function playRound(humanChoice, computerChoice) {
+    let winner = document.getElementById("winner-tag");
+    weHaveAwinner(humanScore, computerScore);
     if(humanChoice === computerChoice) {
-        alert("Draw!!");
+        // alert("Draw!!");
+        winner.textContent = "It`s a draw!";
     }
     else if(
         (humanChoice === "rock" && computerChoice === "scissors") ||
         (humanChoice === "paper" && computerChoice === "rock") ||
         (humanChoice === "scissors" && computerChoice === "paper")
     ) {
-        alert("Human wins this round");
+        // alert("Human wins this round");
         humanScore++;
+        p1Score.textContent = humanScore;
+        winner.textContent = "Human wins";
         console.log(`HumanScore: ${humanScore}, ComputerScore: ${computerScore}`);
+
     } else {
         computerScore++;
-        alert("Computer wins this round");
+        p2Score.textContent = computerScore;
+        winner.textContent = "Computer wins";
+        // alert("Computer wins this round");
         console.log(`HumanScore: ${humanScore}, ComputerScore: ${computerScore}`);
     }
 }
 
-console.log("Loading game...");
+// console.log("Loading game...");
 
-while(humanScore <= 2 && computerScore <= 2) {
-    let human = getHumanChoice();
-    while(human === null || human.length < 3) {
-        human = getHumanChoice();
-    }
-    human = human.toLowerCase();
-    let computerChoice = getComputerChoice();
-    playRound(human, computerChoice);
-    if(humanScore === 2) {
-        console.log("Human Wins");
-        break;
-    } else if(computerScore === 2) {
-        console.log("Computer Wins");
-        break;
-    }
-}
-console.log("Thanks for playing!");
+// while(humanScore <= 2 && computerScore <= 2) {
+//     let human = getHumanChoice();
+//     while(human === null || human.length < 3) {
+//         human = getHumanChoice();
+//     }
+//     human = human.toLowerCase();
+//     let computerChoice = getComputerChoice();
+//     playRound(human, computerChoice);
+//     if(humanScore === 2) {
+//         console.log("Human Wins");
+//         break;
+//     } else if(computerScore === 2) {
+//         console.log("Computer Wins");
+//         break;
+//     }
+// }
+// console.log("Thanks for playing!");
